2a5c674ca74510cdc05344104088eda90a32f8a3
cristianworth <cristianweissmantel@gmail.com>
2021-11-04 13:51:58 -0300

    Add get movies by year and DTO;

--- Repository/MovieRepository.cs
+++ Repository/MovieRepository.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using NHibernate;
 using Factory.MovieRevenue;
 using MovieRevenue.Factory.Entity;
+using MovieRevenue.Factory.DTO;
+using NHibernate.Transform;
 
 namespace MovieRevenue.Repository
 {
@@ -23,6 +25,20 @@ namespace MovieRevenue.Repository
             }
         }
 
+        public IList<MovieDTO> GetAllMoviesByYear(int year)
+        {
+            using (ISession session = SessionFactory.OpenSession())
+            {
+                string sql = @"
+                    SELECT m.movie_id, m.title, m.year_release, m.revenue, s.name as studio_name
+                    FROM Movie as m
+                    LEFT JOIN Studio as s on (m.studio_id = s.studio_id)
+                    WHERE year_release = :year";
 
+                var query = session.CreateSQLQuery(sql);
+                query.SetParameter("year", year);
+                return query.SetResultTransformer(Transformers.AliasToBean<MovieDTO>()).List<MovieDTO>();
+            }
+        }
     }
 }
